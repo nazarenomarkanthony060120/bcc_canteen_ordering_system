edfbec2a419f57b3d913fd99d59d053f04a1a012
nazarenomarkanthony060120 <nazarenomarkanthony060120@gmail.com>
2025-06-26 11:42:34 +0800

    Fix

--- features/common/components/timeout/TimeOut.tsx
+++ features/common/components/timeout/TimeOut.tsx
@@ -6,7 +6,7 @@ const TimeOut = () => {
   return (
     <View className="flex-1 justify-center items-center gap-5 bg-emerald-300">
       <Typo className="text-3xl font-semibold text-red-500">
-        Please Pay your remaining balance to the developer!.
+        Contact the developer. Something error happened!.
       </Typo>
       <Typo className="text-xl">Thank You</Typo>
     </View>
